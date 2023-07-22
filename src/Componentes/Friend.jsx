8be5239562a3ac24f58a740805a52bc45467bda3
matiasfsd7 <matias.n.ortega@gmail.com>
2023-07-22 18:53:45 -0300

    agregue logica

--- src/Componentes/Friend.jsx
+++ src/Componentes/Friend.jsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 
 const Friend = ({ amigo }) => {
-  const [change, setChange] = useState("(from changed state)");
+  const [change, setChange] = useState(" ");
 
   return (
     <div>
-      <h1 className="text-center text-light"> Hello {amigo} </h1>
-      <Button variant="success" onClick={() => setChange(change)}>
+      <h1 className="text-center text-light">
+        {" "}
+        Hello {amigo} {change}{" "}
+      </h1>
+      <Button
+        variant="success"
+        onClick={() =>
+          setChange(change === true) ? "(from changed state)" : " "
+        }
+      >
         Change State
       </Button>{" "}
     </div>
