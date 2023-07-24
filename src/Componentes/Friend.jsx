39309369f005704281ae2b061814046aaf6b1611
matiasfsd7 <matias.n.ortega@gmail.com>
2023-07-24 14:52:19 -0300

    termine el componente friend

--- src/Componentes/Friend.jsx
+++ src/Componentes/Friend.jsx
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 
 const Friend = ({ amigo }) => {
-  const [change, setChange] = useState(" ");
+  const [change, setChange] = useState("");
 
   return (
     <div>
       <h1 className="text-center text-light">
-        {" "}
-        Hello {amigo} {change}{" "}
+        Hello {amigo} {change}
       </h1>
       <Button
+        className="m-5"
         variant="success"
-        onClick={() =>
-          setChange(change === true) ? "(from changed state)" : " "
-        }
+        onClick={() => setChange("(from changed state)")}
       >
         Change State
-      </Button>{" "}
+      </Button>
     </div>
   );
 };
